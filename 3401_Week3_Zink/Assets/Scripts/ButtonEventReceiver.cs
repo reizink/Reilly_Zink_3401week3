4f19762be222503ba701c25deb2f8b9c8b6c6590
trippingoverbothfeet <reizink@yahoo.com>
2019-01-29 17:27:58 -0500

    testing UI and Audio

---
+++ 3401_Week3_Zink/Assets/Scripts/ButtonEventReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEventReceiver : MonoBehaviour {
+
+
+    public void OnButtonPressed()
+    {
+        Debug.Log("Yay. Button pressed...");
+    }
+
+    public void OnButtonDown ()
+    {
+        Debug.Log("Yay. Button pressed down...");
+    }
+    public void OnButtonUp ()
+    {
+        Debug.Log("Yay. Button up...");
+    }
+}
